e1c14f2b59175282715621e824778b42faaeb8c4
Ronaldo Jimenez <ronaldo.jimenez@voltor.org>
2025-12-04 10:12:55 +0100

    quitar create y edit de list

--- football-ui/src/app/equipos-list/equipos-list.ts
+++ football-ui/src/app/equipos-list/equipos-list.ts
@@ -1,30 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { EquiposService } from '../services/equipos';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-equipos-list',
-  imports: [RouterModule, CommonModule],
   templateUrl: './equipos-list.html',
+  imports: [RouterModule],
 })
 export class EquiposList {
-
   private service = inject(EquiposService);
 
   equipos = this.service.equipos;
-
-  loading = signal(false);
-  error = signal('');
-
-  create() {
-    // aquí luego haremos navegación / modal
-    console.log('Crear equipo');
-  }
-
-  edit(id: number) {
-    console.log('Editar', id);
-  }
 
   delete(id: string) {
     this.service.delete(id);
