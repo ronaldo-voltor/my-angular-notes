7c65a29b4a6ff6b45bb02cde21c0f86ebbd1ad48
Ronaldo Jimenez <ronaldo.jimenez@voltor.org>
2025-12-04 14:32:09 +0100

    app de gifs

---
+++ gifs-app/src/app/gifs/pages/search-page/search-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [],
+  templateUrl: './search-page.html',
+})
+export default class SearchPage {
+
+}
